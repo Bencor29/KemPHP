3a3adbdbb33f925c215945a15eb0e8411c6c818c
Bencor29 <Bencordu29@gmail.com>
2019-07-30 01:10:49 +0200

    Patched: bad url when not configured

--- helpers/url.php
+++ helpers/url.php
@@ -6,7 +6,7 @@
     $cnf = new ConfigLoader();
     $url = $cnf->load('url');
     if($url['base'] === null) {
-      $url['base'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      $url['base'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
     }
   }
 
