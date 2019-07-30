416dacf1108d219ed810f18881905f1e694f0ecd
Benjamin CORNOU <benjamin@cornou.dev>
2019-07-30 23:08:17 +0200

    Removed: Developper mode

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 
   namespace App\System;
 
-  error_reporting(E_ALL);
-  ini_set('display_errors',1);
-  ini_set('error_reporting', E_ALL);
-  ini_set('display_startup_errors',1);
-  error_reporting(-1);
-
   use App\System\ConfigLoader;
   use App\System\Controller;
   use App\System\Session;
